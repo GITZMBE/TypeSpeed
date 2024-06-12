73c06242d2dcfc0be940de724a0224d2ead44a4e
GITZMBE <lucasfilip@hotmail.se>
2024-06-12 21:38:16 +0200

    Add history to TypingResult

--- src/models/TypingResult.ts
+++ src/models/TypingResult.ts
@@ -2,11 +2,13 @@ class TypingResult {
   time!: number;
   words!: number;
   wrong_words!: number;
+  history: number[] = [];
 
-  constructor(time: number, words: number, wrong_words: number) {
+  constructor(time: number, words: number, wrong_words: number, history: number[]) {
     this.time = time;
     this.words = words;
     this.wrong_words = wrong_words;
+    this.history = history;
   }
 };
 
