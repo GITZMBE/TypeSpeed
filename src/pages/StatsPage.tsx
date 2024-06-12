2134cfeb2589ab91aa23154d51d79e19e1d91471
GITZMBE <lucasfilip@hotmail.se>
2024-06-12 19:16:22 +0200

    Change how currentTypingResult is being saved and layout in pages

--- src/pages/StatsPage.tsx
+++ src/pages/StatsPage.tsx
@@ -1,31 +1,62 @@
 import React, { useEffect, useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import StatsChart from "../components/StatsChart";
+import TypingResult from "../models/TypingResult";
+import { useRecoilState } from "recoil";
+import { TypingResultState } from "../recoil/states";
 
 const StatsPage = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [time, setTime] = useState<string | null>(null);
-  const [words, setWords] = useState<string | null>(null);
-  const [wrongWords, setWrongWords] = useState<string | null>(null);
+  const navigate = useNavigate();
+  const [result, setResult] = useRecoilState(TypingResultState);
 
   useEffect(() => {
-    setTime(searchParams.get('time'));
-    setWords(searchParams.get('words'));
-    setWrongWords(searchParams.get('wrong_words') || '0');
-  }, [searchParams, setSearchParams]);
+    if (!result) {
+      navigate("/");
+    }
+  }, []);
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center gap-8">
-      <h1 className="text-5xl text-yellowAcent font-robotoMono">StatsPage</h1>
-      {(time && words) && (
+    <div className='w-full h-full flex flex-col justify-center items-center gap-8'>
+      <h1 className='text-5xl text-yellowAcent font-robotoMono'>StatsPage</h1>
+      {result && (
         <>
-          <div className="flex items-center gap-4 text-secondary">
-            <span>words: {words}</span>
-            <span>time: {Number(time).toFixed(3)} s</span>
-            <span className={Number(wrongWords) > 1 ? "text-red-500" : Number(wrongWords) > 0 ? "text-yellow-500" : "text-green-500"}>wrong words: {Number(wrongWords)}</span>
-            <span className="text-2xl">accuracy: {(100 * (Number(words) - Number(wrongWords)) / Number(words)).toFixed(3)}%</span>
+          {/* <StatsChart data={[new TypingResult(time, words, wrongWords)]} /> */}
+          <div className='flex items-center gap-4 text-secondary'>
+            <span>words: {result.words}</span>
+            <span>time: {result.time.toFixed(3)} s</span>
+            <span
+              className={
+                result.wrong_words > 1
+                  ? "text-red-500"
+                  : result.wrong_words > 0
+                  ? "text-yellow-500"
+                  : "text-green-500"
+              }
+            >
+              wrong words: {result.wrong_words}
+            </span>
+            <span className='text-2xl'>
+              accuracy:{" "}
+              {(
+                (100 * (result.words - result.wrong_words)) /
+                result.words
+              ).toFixed(3)}
+              %
+            </span>
           </div>
-          <p className="font-bold text-xl text-secondary">{(60 * (Number(words) - Number(wrongWords)) / Number(time)).toFixed(3)} wpm</p>
-          <Link to="/typing" className="text-5xl text-secondary hover:text-light">Try again</Link>
+          <p className='font-bold text-xl text-secondary'>
+            {((60 * (result.words - result.wrong_words)) / result.time).toFixed(
+              3
+            )}{" "}
+            wpm
+          </p>
+          <Link
+            to='/typing'
+            onClick={() => setResult(null)}
+            className='text-5xl text-secondary hover:text-light'
+          >
+            Try again
+          </Link>
         </>
       )}
     </div>
--- src/pages/TypePage.tsx
+++ src/pages/TypePage.tsx
@@ -1,12 +1,27 @@
 import React, { useEffect, useRef, useState } from "react";
-import LetterCoordinate from "../classes/LetterCoordinate";
+import LetterCoordinate from "../models/LetterCoordinate";
 import { useNavigate } from "react-router-dom";
 import Letter from "../components/Letter";
+import TypingResult from "../models/TypingResult";
+import { useSetRecoilState } from "recoil";
+import { TypingResultState } from "../recoil/states";
+import DIFFECULTY from "../models/DIFFECULTY";
+import TypingSettings from "../models/TypingSettings";
+import HorizontalDivider from "../components/HorizontalDivider";
+import { TbSquareLetterA } from "react-icons/tb";
+import { MdAccessTimeFilled } from "react-icons/md";
+var randomWord = require("random-word-by-length");
 
 const TypePage = () => {
   const navigate = useNavigate();
-  const [sentenceLength, setSentenceLength] = useState<number | null>(null);
-  const [selectedTime, setSelectedTime] = useState<number | null>(null);
+  const setResult = useSetRecoilState(TypingResultState);
+  const [settings, setSettings] = useState<TypingSettings>({
+    difficulty: DIFFECULTY.NORMAL,
+    wordsAmount: null,
+    selectedTime: null,
+  });
+  const [settingsType, setSettingsType] = useState<string>("words");
+  const [testHasStarted, setTestHasStarted] = useState(false);
   const [time, setTime] = useState<number | null>(null);
   const [words, setWords] = useState<string[]>([]);
   const [displayWords, setDisplayWords] = useState<string[]>([]);
@@ -34,28 +49,39 @@ const TypePage = () => {
 
   useEffect(() => {
     if (words.length > 0 && displayWords.length > 0) return;
-    FetchRandomWords().then(res => {
-      setWords(res); 
-      return res;
-    }).then(w => {
-      w.length > 0 && setDisplayWords(w)
-    });
+    FetchRandomWords()
+      .then((res) => {
+        setWords(res);
+        setDisplayWords(res);
+      });
   }, [words, displayWords]);
-  
+
   const startTimer = () => {
-    if (!selectedTime || !time) return;
+    if (!settings.selectedTime || !time) return;
 
     let t = 1;
     const interval = setInterval(() => {
-      if (time === t) {
-        navigate(`/stats?time=${selectedTime}&words=${wordIndexRef.current}&wrong_words=${calculateWrongWords(wrongLettersRef.current)}`);
-        setToDefault();
+      if (time === t && settings.selectedTime) {
+        finishTest();
         clearInterval(interval);
         return;
       }
-      setTime(prevTime => prevTime !== null ? prevTime - 1 : null);
+      setTime((prevTime) => (prevTime !== null ? prevTime - 1 : null));
       t++;
     }, 1000);
+  };
+
+  const startTest = () => {
+    if (wordIndex === 0 && letterIndex === 0) {
+      if (settings.wordsAmount) {
+        setResult(null);
+        setStartTime(new Date().getTime());
+        setTestHasStarted(true);
+      } else if (settings.selectedTime) {
+        startTimer();
+        setTestHasStarted(true);
+      }
+    }
   };
 
   useEffect(() => {
@@ -74,16 +100,10 @@ const TypePage = () => {
 
     function KeyPress(e: any): void {
       const pressedKey = e.key;
-      const activeWord = words[wordIndex];
+      const activeWord = displayWords[wordIndex];
       const activeLetter = activeWord[letterIndex];
 
-      if (wordIndex === 0 && letterIndex === 0) {
-        if (sentenceLength) {
-          setStartTime(new Date().getTime());
-        } else if (selectedTime) {
-          startTimer();
-        }
-      }
+      startTest();
 
       if (letterIndex < activeWord.length) {
         setLetterIndex(letterIndex + 1);
@@ -150,20 +170,22 @@ const TypePage = () => {
   }, [searchbarFocus, wordIndex, letterIndex]);
 
   useEffect(() => {
-    if (sentenceLength) {
+    if (settings.wordsAmount) {
       if (
         wordIndex === displayWords.length - 1 &&
         letterIndex === displayWords[displayWords.length - 1].length
       ) {
         setEndTime(new Date().getTime());
       }
-    }  
+    }
   }, [wordIndex, letterIndex]);
 
   const calculateWrongWords = (wrongLetters: LetterCoordinate[]) => {
     let wrongWords: LetterCoordinate[] = [];
     for (const letterCoord of wrongLetters) {
-      const isOtherLetterInWord = wrongWords.some(c => c.wordCoord === letterCoord.wordCoord);
+      const isOtherLetterInWord = wrongWords.some(
+        (c) => c.wordCoord === letterCoord.wordCoord
+      );
       if (isOtherLetterInWord) {
         continue;
       }
@@ -173,8 +195,11 @@ const TypePage = () => {
   };
 
   const setToDefault = () => {
-    setSentenceLength(null);
-    setSelectedTime(null);
+    setSettings({
+      difficulty: DIFFECULTY.NORMAL,
+      wordsAmount: null,
+      selectedTime: null,
+    });
     setTime(null);
     setWords([]);
     setWordIndex(0);
@@ -186,33 +211,77 @@ const TypePage = () => {
     setEndTime(null);
   };
 
-  useEffect(() => {
+  const finishTest = () => {
     if (startTime && endTime) {
       const duration = (endTime - startTime) / 1000;
-      navigate(`/stats?time=${duration}&words=${displayWords.length}&wrong_words=${calculateWrongWords(wrongLetters)}`);
+      setResult(
+        new TypingResult(
+          duration,
+          displayWords.length,
+          calculateWrongWords(wrongLetters)
+        )
+      );
+      navigate(`/stats`);
       setToDefault();
+      return;
+    };
+    if (settings.selectedTime) {
+      setResult(
+        new TypingResult(
+          settings.selectedTime,
+          wordIndexRef.current,
+          calculateWrongWords(wrongLettersRef.current)
+        )
+      );
+      navigate(`/stats`);
+      setToDefault();
+      return;
     }
+  };
+
+  useEffect(() => {
+    finishTest();
   }, [startTime, endTime]);
 
   useEffect(() => {
-    if (!sentenceLength) return;
+    FetchRandomWords().then((res) => {
+      setWords(res);
+      return res;
+    });
+    if (!settings.wordsAmount) {
+      setDisplayWords(words);
+      return;
+    }
     const newWordArray = [];
-    for (let i = 0; i < sentenceLength; i++) {
+    for (let i = 0; i < settings.wordsAmount; i++) {
       newWordArray.push(words[i]);
     }
     setDisplayWords(newWordArray);
-  }, [sentenceLength]);
+  }, [settings]);
 
   const FetchRandomWords = async (): Promise<string[]> => {
     try {
-      const url: string = "https://random-word-api.herokuapp.com/word?number=100";
-      const response = await fetch(url);
-      const words = await response.json();
-      return words;
+      const wordsArray: string[] = [];
+      for (const _ of new Array(100)) {
+        const random: string = randomWord(
+          settings.difficulty === DIFFECULTY.EASY
+            ? 3
+            : settings.difficulty === DIFFECULTY.NORMAL
+            ? 6
+            : settings.difficulty === DIFFECULTY.HARD
+            ? undefined
+            : undefined
+        );
+        wordsArray.push(random);
+      }
+      return wordsArray;
     } catch (error) {
       return [];
     }
   };
+
+  // const wpm = startTime ? (60 * wordIndex / ((new Date().getTime() - startTime) / 1000 / 60)) : undefined;
+  // const cpm = startTime ? (60 * correctLetters.length / ((new Date().getTime() - startTime) / 1000 / 60)) : undefined;
 
   return (
     <div
@@ -221,41 +290,129 @@ const TypePage = () => {
       }}
       className='w-full max-w-7xl h-full flex flex-col gap-8 justify-center items-center'
     >
-      <div className="w-full flex justify-between items-center">
-        <div className="flex">
-          {
-            [10, 30, 60, null].map((leng) => (
-              <button key={leng} className={`text-xl ${ time === leng ? "text-yellowAcent" : "text-secondary hover:text-light" } p-2 text-nowrap`} onClick={() => leng === null ? (setSelectedTime(leng), setTime(leng)) : (setSelectedTime(leng), setTime(leng), setSentenceLength(null))}>{ leng === null ? "no limit" : leng }</button>
-            ))
-          }
-        </div>
-        { time && (
-          <div className="text-3xl text-secondary">
-            {time}
+      <div className='w-full flex flex-col gap-4'>
+        <div className='w-full flex flex-col justify-center items-center text-xl text-secondary'>
+          <h2>Current Speed</h2>
+          <div className='flex gap-8 items-center'>
+            {/* <p>{wpm ? wpm.toFixed(2) : undefined} wpm</p>
+            <p>{cpm ? cpm.toFixed(2) : undefined} cpm</p> */}
           </div>
-        )}
-        <div className="flex">
-          {
-            [10, 15, 20, null].map((leng) => (
-              <button key={leng} className={`text-xl ${ sentenceLength === leng ? "text-yellowAcent" : "text-secondary hover:text-light" } p-2 text-nowrap`} onClick={() => leng === null ? setSentenceLength(leng) : (setSentenceLength(leng), setSelectedTime(null), setTime(null))}>{ leng === null ? "no limit" : leng }</button>
-            ))
-          }
         </div>
+          <div className={`w-full flex transition-opacity duration-300 ${testHasStarted && "opacity-0"}`}>
+            <div className='flex justify-center items-center'>
+              {[DIFFECULTY.EASY, DIFFECULTY.NORMAL, DIFFECULTY.HARD].map((d) => (
+                <button
+                  key={d}
+                  className={`text-xl ${
+                    settings.difficulty === d
+                      ? "text-yellowAcent"
+                      : "text-secondary hover:text-light"
+                  } p-2 text-nowrap`}
+                  onClick={() => setSettings({ ...settings, difficulty: d })}
+                >
+                  {DIFFECULTY[d]}
+                </button>
+              ))}
+            </div>
+            <HorizontalDivider />
+            <div className="flex flex-grow">
+              {["time", "words"].map(type => (
+                <button key={type} onClick={() => setSettingsType(type)} className={`flex items-center gap-2 text-xl ${ settingsType === type ? "text-yellowAcent" : "text-secondary hover:text-light"} p-2`}>{type === "words" ? (<TbSquareLetterA size={24} />) : type === "time" ? (<MdAccessTimeFilled size={24} />) : (<></>)} {type}</button>
+              ))}
+            </div>
+            <HorizontalDivider />
+            {settingsType === "time" ? (
+              <div className='flex justify-center items-center'>
+                {[10, 30, 60, null].map((leng) => (
+                  <button
+                    key={leng}
+                    className={`text-xl ${
+                      settings.selectedTime === leng
+                        ? "text-yellowAcent"
+                        : "text-secondary hover:text-light"
+                    } p-2 text-nowrap`}
+                    onClick={() =>
+                      leng === null
+                        ? (setSettings((prevSettings) => ({
+                            ...prevSettings,
+                            selectedTime: leng,
+                          })),
+                          setTime(leng))
+                        : (setSettings((prevSettings) => ({
+                            ...prevSettings,
+                            selectedTime: leng,
+                          })),
+                          setTime(leng),
+                          setSettings((prevSettings) => ({
+                            ...prevSettings,
+                            wordsAmount: null,
+                          })))
+                    }
+                  >
+                    {leng === null ? "No limit" : leng}
+                  </button>
+                ))}
+              </div>
+            ) : settingsType === "words" ? (
+              <div className='flex justify-center items-center'>
+                  {[10, 15, 20, null].map((leng) => (
+                    <button
+                      key={leng}
+                      className={`text-xl ${
+                        settings.wordsAmount === leng
+                          ? "text-yellowAcent"
+                          : "text-secondary hover:text-light"
+                      } p-2 text-nowrap`}
+                      onClick={() =>
+                        leng === null
+                          ? setSettings((prevSettings) => ({
+                              ...prevSettings,
+                              wordsAmount: leng,
+                            }))
+                          : (setSettings((prevSettings) => ({
+                              ...prevSettings,
+                              wordsAmount: leng,
+                            })),
+                            setSettings((prevSettings) => ({
+                              ...prevSettings,
+                              selectedTime: null,
+                            })),
+                            setTime(null))
+                      }
+                    >
+                      {leng === null ? "No limit" : leng}
+                    </button>
+                  ))}
+              </div>
+            ) : (<></>)}
+          </div>
       </div>
+      {(testHasStarted && time) && <div className='w-full text-3xl text-yellowAcent'>{time}</div>}
       <div
         onClick={(e: any) => {
-          if (sentenceLength || selectedTime) {
+          if (settings.wordsAmount || settings.selectedTime) {
             setSearchbarFocus(true);
             e.stopPropagation();
           }
         }}
         className='w-full h-64 flex overflow-y-hidden'
       >
-        <div className="flex flex-wrap h-fit">
+        <div className='flex flex-wrap h-fit'>
           {displayWords.map((word, i) => (
             <div key={i} className='word h-fit flex gap-[1px] m-2'>
               {word.split("").map((letter, j) => (
-                <Letter key={j} correctLetters={correctLetters} wrongLetters={wrongLetters} word={word} wordIndex={wordIndex} letterIndex={letterIndex} currentWordIndex={i} currentLetterIndex={j} searchbarFocus={searchbarFocus} letter={letter} />
+                <Letter
+                  key={j}
+                  correctLetters={correctLetters}
+                  wrongLetters={wrongLetters}
+                  word={word}
+                  wordIndex={wordIndex}
+                  letterIndex={letterIndex}
+                  currentWordIndex={i}
+                  currentLetterIndex={j}
+                  searchbarFocus={searchbarFocus}
+                  letter={letter}
+                />
               ))}
             </div>
           ))}
