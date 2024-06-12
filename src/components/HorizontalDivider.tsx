7171420b90013ba5209f92dff300cf40f866fde6
GITZMBE <lucasfilip@hotmail.se>
2024-06-12 19:15:03 +0200

    Create HorizontalDivider (<hr/> but horizontal)

---
+++ src/components/HorizontalDivider.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HorizontalDivider = () => {
+  return (
+    <div className="border-x-[1px] border-secondary m-2"></div>
+  )
+}
+
+export default HorizontalDivider;
