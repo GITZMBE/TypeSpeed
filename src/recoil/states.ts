0eb6d1dac9d1766ac45fe60f1ecab6fcc236a775
GITZMBE <lucasfilip@hotmail.se>
2024-06-13 09:10:28 +0200

    Make settings to a global state

--- src/recoil/states.ts
+++ src/recoil/states.ts
@@ -1,7 +1,18 @@
 import { atom } from "recoil";
 import TypingResult from "../models/TypingResult";
+import TypingSettings from "../models/TypingSettings";
+import DIFFECULTY from "../models/DIFFECULTY";
 
 export const TypingResultState = atom<TypingResult | null>({
-  key: "TypeResultState",
+  key: "TypingResultState",
   default: null,
+});
+
+export const SettingsState = atom<TypingSettings>({
+  key: "SettingsState",
+  default: {
+    difficulty: DIFFECULTY.NORMAL,
+    wordsAmount: null,
+    selectedTime: null,
+  }
 });
