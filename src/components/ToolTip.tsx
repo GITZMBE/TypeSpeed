7d48fae64fe1f0fec91cd90ae62a084be03e325d
GITZMBE <lucasfilip@hotmail.se>
2024-06-13 10:54:52 +0200

    Create ToolTip component for Tippy with designed theme

---
+++ src/components/ToolTip.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement } from 'react';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/translucent.css';
+import 'tippy.js/animations/shift-away.css';
+
+interface IProps {
+  children: ReactElement;
+  content: ReactElement;
+}
+
+const ToolTip = ({ children, content }: IProps) => {
+  return (
+    <Tippy theme='translucent' content={content} animation="shift-away">
+      { children }
+    </Tippy>
+  )
+}
+
+export default ToolTip;
