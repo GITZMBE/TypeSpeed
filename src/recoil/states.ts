dacbc1b86448e2bed7aadcef7e2bb37d5ab8e3b2
GITZMBE <lucasfilip@hotmail.se>
2024-06-13 15:29:39 +0200

    Create recoil state for current user

--- src/recoil/states.ts
+++ src/recoil/states.ts
@@ -2,6 +2,7 @@ import { atom } from "recoil";
 import TypingResult from "../models/TypingResult";
 import TypingSettings from "../models/TypingSettings";
 import DIFFECULTY from "../models/DIFFECULTY";
+import { User } from "@prisma/client";
 
 export const TypingResultState = atom<TypingResult | null>({
   key: "TypingResult",
@@ -15,4 +16,9 @@ export const SettingsState = atom<TypingSettings>({
     wordsAmount: 10,
     selectedTime: null,
   }
+});
+
+export const CurrentUserState = atom<User | null>({
+  key: "CurrentUser",
+  default: null
 });
