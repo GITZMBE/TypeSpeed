705423582a05ca373486923118fa32b6bad25e6b
GITZMBE <lucasfilip@hotmail.se>
2024-06-14 08:27:37 +0200

    Write documention for speedHandler functions

--- src/utils/speedHandler.ts
+++ src/utils/speedHandler.ts
@@ -1,14 +1,33 @@
 
-
+/**
+ * 
+ * @param entries The total entries (correct + wrong)
+ * @param time The time in minutes
+ * @returns gross wpm in type number
+ */
 export const calcGrossWpm = (entries: number, time: number) => {
   return entries / 5 / time;
 };
 
+/**
+ * 
+ * @param entries The total entries (correct + wrong)
+ * @param time The time in minutes
+ * @param errors Uncorrected entries
+ * @returns wpm in type number
+ */
 export const calcNetWpm = (entries: number, time: number, errors: number) => {
   const gross = calcGrossWpm(entries, time);
   return gross - (errors / time);
 };
 
+/**
+ * 
+ * @param entries The total entries (correct + wrong)
+ * @param time The time in minutes
+ * @param errors Uncorrected entries
+ * @returns accuracy in percentage
+ */
 export const calcAccuracy = (entries: number, time: number, errors: number) => {
   const gross = calcGrossWpm(entries, time);
   const net = calcNetWpm(entries, time, errors);
